c194c2766bcc1b300e7588b9a37c0889a7628366
Cat Hanbit <dreamlmy1203@naver.com>
2022-11-14 19:28:38 +0900

    first commit

--- .history/pages/index_20221101095221.js
+++ .history/pages/index_20221101095221.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { client } from '../lib/client';
-import { Product, FooterBanner, HeroBanner } from '../components';
-
+import { client }  HeroBanner } from '../components';
+from '../lib/client';
+import { Product, FooterBanner,
 const Home = ({ products, bannerData }) => (
   <div>
     <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
